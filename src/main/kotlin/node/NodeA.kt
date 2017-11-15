7bdf6c389498af31aa80f42cd3f7ba3557a7d983
joeldudley <joeldudley@outlook.com>
2017-11-15 22:27:58 +0000

    Makes one node a webserver.

--- src/main/kotlin/node/NodeA.kt
+++ src/main/kotlin/node/NodeA.kt
@@ -1,14 +1,29 @@
 package node
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.ServerSocket
+
+
 fun main(args: Array<String>) {
     NodeA.doStuff()
 }
 
 object NodeA {
     fun doStuff() {
+        // TODO: Make sure everything gets closed.
+
+        val socket = ServerSocket(4444)
+
+        // TODO: Make multithreaded.
         while (true) {
-            println("hA")
-            Thread.sleep(1_000)
+            val connection = socket.accept()
+            val inReader = BufferedReader(InputStreamReader(connection.getInputStream()))
+
+            while (true) {
+                val line = inReader.readLine() ?: break
+                println(line)
+            }
         }
     }
 }
