11e116971f0523aeeff6a91d7293c39036af1c01
joeldudley <joeldudley@outlook.com>
2017-12-21 22:58:27 +0000

    Adds comments.

--- src/main/kotlin/server/Router.kt
+++ src/main/kotlin/server/Router.kt
@@ -3,22 +3,50 @@ package server
 import server.request.Method
 import server.request.Request
 
+/**
+ * Dictates how the server handles specific HTTP requests.
+ *
+ * Individual [Route]s are installed using the [registerRoute] method.
+ */
 class Router {
     private val routes = mutableMapOf<Pair<String, Method>, Route>()
 
+    /**
+     * Adds a [Route] to the router.
+     *
+     * @param path the path the route handles.
+     * @param method the HTTP method the route handles.
+     * @param route how to respond to the request.
+     */
     fun registerRoute(path: String, method: Method, route: Route) {
         routes[path to method] = route
     }
 
-    // TODO: Introduce error handlers for unrecognised routes
-
+    /**
+     * Routes an HTTP request to the correct [Route].
+     *
+     * @param request the HTTP request.
+     * @param connection the connection to the client.
+     */
     fun handleConnection(request: Request, connection: ClientConnection) {
+        // TODO: Introduce error handlers for unrecognised routes
         val route = routes[request.path to request.method] ?: throw UnregisteredRouteException()
         route.dispatch(request, connection)
     }
 }
 
+/**
+ * Represents an individual route.
+ *
+ * The route defines how an HTTP request is handled by overriding [dispatch].
+ */
 abstract class Route {
+    /**
+     * Handles an HTTP request.
+     *
+     * @param request the HTTP request.
+     * @param connection the connection to the client.
+     */
     abstract fun dispatch(request: Request, connection: ClientConnection)
 }
 
--- src/main/kotlin/server/Server.kt
+++ src/main/kotlin/server/Server.kt
@@ -5,13 +5,27 @@ import java.net.ServerSocket
 import java.net.SocketException
 import java.util.concurrent.Executors
 
-/** A server that echoes what's written to the [socket]. */
-class Server(private val socket: Int, numberOfThreads: Int = 10) {
-    // A socket that listens for localhost connections.
-    private val serverSocket = ServerSocket(socket)
+/**
+ * A server.
+ *
+ * @param port The port the server listens on.
+ * @param numberOfThreads The number of threads the server uses to handle requests.
+ */
+class Server(private val port: Int, numberOfThreads: Int = 10) {
+    // The server socket listens for localhost connections on the specified port.
+    private val serverSocket = ServerSocket(port)
+    // The threads for handling HTTP requests.
     private val threadPool = Executors.newFixedThreadPool(numberOfThreads)
+    // Prepares a response to HTTP requests based on their path.
     private val router = Router()
 
+    /**
+     * Adds a route to the router.
+     *
+     * @param path the path the route handles.
+     * @param method the HTTP method the route handles.
+     * @param route how to respond to the request.
+     */
     fun registerRoute(path: String, method: Method, route: Route) {
         router.registerRoute(path, method, route)
     }
@@ -26,6 +40,7 @@ class Server(private val socket: Int, numberOfThreads: Int = 10) {
                 break
             }
 
+            // Each request is assigned its own thread.
             threadPool.submit {
                 val clientConnection = ClientConnection(connection)
                 val request = clientConnection.parseRequest()
@@ -35,6 +50,7 @@ class Server(private val socket: Int, numberOfThreads: Int = 10) {
         }
     }
 
+    /** Shuts down the server. */
     fun shutDown() {
         serverSocket.close()
     }
